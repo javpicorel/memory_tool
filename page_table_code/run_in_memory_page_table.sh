e59cf82d24ff05ea8d2d23ebd8518036be84079b
root <root@iccluster057.iccluster.epfl.ch>
2016-11-02 20:33:31 +0100

    Write script for running

--- page_table_code/run_in_memory_page_table.sh
+++ page_table_code/run_in_memory_page_table.sh
@@ -15,23 +15,25 @@
 # 5 - Cassandra
 # 6 - Neo4j
 
-workload_path="/home/parsacom/vol/icfiler3-ext/picorel/ASPLOS2017/pin-traces/"
+workload_path="/mnt/raid10/pin-traces/"
 workload_name=( "memcached" "rocksdb" "mysql" "tpch" "tpcds" "cassandra" "neo4j" )
-result_path="/net/parsafiler4/users/picorel/ISCA2017/associativity/"
-memory_sizes=( 8 8 8 32 16 8 8 )
+result_path="/mnt/raid10/results/pagetables/"
+memory_sizes=( 512 512 512 32 16 512 512 )
 memory_tool_sizes=( 8 8 8 32 16 8 12 )
 number_processes=( 1 2 4 8 16 32 64 )
-associativity=( 1 2 4 8 16 32 64 128 )
-operations=1000000000
+factors=( 2 4 8 )
+#operations=10000000000
+operations=1000
 page_size=4096
-num_vaults=64
-wp_entries=1024
+num_vaults=(16 16 16 64 32 16 32)
 cores=-1 # -1 all cores
+vault_id=0
 
 echo -e "Workload Path: $workload_path"
 echo -e "Result Path: $result_path"
 
-for i in 0 1 2 3 4 6 # All except Cassandra
+#for i in 0 1 2 3 4 # All except Cassandra and Neo4j
+for i in 0 # All except Cassandra and Neo4j
 do
   echo -e "====================================="
   echo -e "Workload: ${workload_name[$i]}"
@@ -43,19 +45,15 @@ do
     echo -e "Number of processes: $j"
     echo -e "========================"
 
-    for k in "${associativity[@]}"
+    for k in "${factors[@]}"
     do
 
       #We should run associativities at least equal to the number of processes
-      if [ "$j" -gt "$k" ]
-      then
-        continue
-      fi
       echo -e "================="
-      echo -e "Associativity: $k"
+      echo -e "Factor: $k"
       echo -e "================="
 
-      ./Fast3DCacheImpl $((1024*${memory_tool_sizes[$i]})) $i $page_size $k $cores $operations $workload_path${workload_name[$i]}"/"${memory_sizes[$i]}"GB/" $result_path $num_vaults $wp_entries ${memory_sizes[$i]} $j &
+      ./Fast3DCacheImpl ${memory_sizes[$i]} $i $page_size $cores $operations $workload_path${workload_name[$i]}"/"${memory_tool_sizes[$i]}"GB/" $result_path ${num_vaults[$i]} $vault_id ${memory_tool_sizes[$i]} $j $k > $result_path${workload_name[$i]}"_"$j"x_"${memory_sizes[$i]}"MB_"${memory_tool_sizes[$i]}"GB_"$k"f_"${num_vaults[i]}"v"
     done
 
     wait #Wait for the processes 
